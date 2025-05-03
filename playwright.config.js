ace0c642d054b3c43f502e64a1f321faa0ab58ec
serhii.suzanskyi <serhii.suzanskyi@navinfo.eu>
2025-05-03 10:56:24 +0200

    pipeline update

--- playwright.config.js
+++ playwright.config.js
@@ -29,6 +29,9 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
+    /* Run tests in headed mode locally, but headless in CI */
+    headless: process.env.CI ? true : false,
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
